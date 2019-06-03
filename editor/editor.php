c556cd68d893fbba567bbb971dba9229eb416f96
Charly <charlyzz68@gmail.com>
2019-06-03 17:52:36 -0300

    despues toolbar column

--- editor/editor.php
+++ editor/editor.php
@@ -5,15 +5,23 @@
 	.main, .columnas{
 		list-style: none;
 	}
+
+	/*.column-toolbar{
+		position: absolute;
+		top:0;
+		left: 0;
+		width: 100%;
+		z-index: 100;
+	}*/
 </style>
 
- <ul class="main col-12 flex-col start-center p-0 m-0">
+ <ul class="main col-12 flex-col start-center m-0">
  	<li class="fila col-12 flex-col-start-start p-2">
  		<h3>Título Fila</h3>
- 		<ul class="columnas col-12 flex-row-start-start m-0 p-0">
+ 		<ul class="columnas col-12 flex-row-start-start m-0">
  			<?php for($i = 0; $i < 4; $i++) { ?>
- 			<li class="column col-12 col-md-6 col-lg-3 p-0">
- 				columna
+ 			<li class="column col-12 col-md-6 col-lg-3">
+ 				<!-- <div class="column-toolbar flex-row-between-center bg-light elevation-1"></div> -->
  			</li>
  			<?php } ?>
  		</ul>
@@ -22,4 +30,3 @@
 
 
 
- 
